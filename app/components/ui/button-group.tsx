eead6cfe8885150cfa47599c6c3533a6a129e8ef
paulhmorris <paulh.morris@gmail.com>
2023-10-19 13:34:10 -0500

    style: update button-group alignment

--- app/components/ui/button-group.tsx
+++ app/components/ui/button-group.tsx
@@ -3,7 +3,7 @@ import { cn } from "~/lib/utils";
 
 export function ButtonGroup({ className, children, ...props }: ComponentPropsWithoutRef<"div">) {
   return (
-    <div className={cn("flex flex-col-reverse gap-2 sm:flex-row sm:justify-end", className)} {...props}>
+    <div className={cn("flex flex-col-reverse gap-2 sm:flex-row", className)} {...props}>
       {children}
     </div>
   );
