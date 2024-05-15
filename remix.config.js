4bcf3649e8ec0e5fb8da2491b03f0b80b2382164
Paul Morris <paulh.morris@gmail.com>
2024-05-15 11:56:45 -0500

    Bundle nanoid on server

--- remix.config.js
+++ remix.config.js
@@ -3,5 +3,5 @@ module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.test.{js,jsx,ts,tsx}"],
   serverModuleFormat: "cjs",
-  serverDependenciesToBundle: [/remix-utils/],
+  serverDependenciesToBundle: [/remix-utils/, /nanoid/],
 };
